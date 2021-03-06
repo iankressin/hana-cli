0057de51445cc5777e84fa77439293499140ea5e
Ian <ianguimaraes31@gmail.com>
2021-03-06 02:23:43 -0300

    Update installer

--- install.sh
+++ install.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-
-DRIVE_DIR="$HOME/.hana"
+HANA_DIR="$HOME/.hana"
 
 function install {
-  echo "Installing Local Drive ..."
+  echo "Installing Hana CLI ..."
 
-  URL="https://github.com/iankressin/hana-cli/releases/download/0.0.2/drive-cli"
+  LATEST=$(curl -qs https://api.github.com/repos/iankressin/hana-cli/releases/latest | grep tag_name | head -n 1 | cut -d '"' -f 4);
+  URL="https://github.com/iankressin/hana-cli/releases/download/${LATEST}/hana-cli"
+
+  echo $URL
 
   echo "--> Configuring directories ..."
-  mkdir $DRIVE_DIR
-  cd $DRIVE_DIR
+  mkdir $HANA_DIR
+  cd $HANA_DIR
 
   echo "--> Downloading ..."
   bash -c "ls"
-  bash -c "curl --fail -# -L $URL > drive"
-  BIN="drive"
+  bash -c "curl --fail -# -L $URL > hana"
+  BIN="hana"
 	  chmod +x $BIN || fail "chmod +x failed"
 
   echo "*** DONE ***"
-  echo "Now, please add $DRIVE_DIR to your PATH"
+  echo "Now, please add $HANA_DIR to your PATH"
 }
 
 install
